ae71693df36dbae3f07af97df03edff916939d1e
Palak <palakkhuntia28@gmail.com>
2026-07-23 22:44:49 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Palak Khuntia',
+    author_email= 'palakkhuntia28@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
